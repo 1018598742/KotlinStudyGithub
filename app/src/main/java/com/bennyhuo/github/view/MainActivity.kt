cce2d71eb0ad477d853b74043e81409bbbda0fd8
fta <1018598742@qq.com>
2020-01-08 20:57:40 +0800

    fix:in a mess

--- app/src/main/java/com/bennyhuo/github/view/MainActivity.kt
+++ app/src/main/java/com/bennyhuo/github/view/MainActivity.kt
@@ -2,19 +2,32 @@ package com.bennyhuo.github.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.bennyhuo.github.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
-import org.jetbrains.anko.toast
-import java.text.SimpleDateFormat
+import androidx.navigation.ui.AppBarConfiguration as AppBarConfiguration1
 
 
-class MainActivity :AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        appBarConfiguration = AppBarConfiguration1(
+            topLevelDestinationIds = setOf(
+                R.id.navRepos,
+                R.id.navPeople,
+                R.id.navIssue,
+                R.id.navAbout
+            ),
+            drawerLayout = drawer_layout
+        )
 
     }
 
